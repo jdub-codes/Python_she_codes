87302fe5f2b4ac625131b046655f9694eb92a857
jdub-codes <jenny.waller@riotinto.com>
2023-05-30 20:29:31 +1000

    Session 1 Python code

---
+++ Session_1/variables.py
@@ -0,0 +1,66 @@
+# Create a variable named first name and assign Alsi to it
+first_name = "Asli"
+last_name = "Yoruk"
+programming_languge = "Python"
+
+# Data Types - Text data and numerical data
+# Text data - String
+today = "Tuesday"
+# print(today)
+# print(f"Today is {today}")
+# print(type(today))
+
+# Numerical data types - Integer, Float
+# Integer - whole number
+parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+# height = 157
+# print(f"My height is {height/100} in m.")
+
+#Float
+weight = 47.6
+# print(type(weight))
+# print(f"My weight is {weight*2.1} in lbs")
+
+distance = "5000"
+# print(int(distance) + 8)
+# print(distance + str(8))
+
+#print("hello")
+# dog_name = input("What's your name?")
+# print("Nice to meet {dog_name}")
+
+# Input function stores the data as string
+# birth_year = input("What year were you born?")
+# print(f"You are {2023-int(birth_year)} years old.")
+
+# Write a program that takes two numbers from the user,and outputs their sum
+# first_number = input("What is your first number?")
+# second_number = input("What is your second number?")
+# print(f"The sum of your numbers is {int(first_number)+int(second_number)}")
+
+# Write a program that takes two numbers from the user,and outputs the equation representing the multiplication of the two numbers.
+# first_number = input("What is your first number?")
+# second_number = input("What is your second number?")
+# print(f"The sum of your numbers is {int(first_number)*int(second_number)}")
+
+# Write a program that takes a distance in kilometers from the user, and outputs the distance in meters and centimeters.
+# distance = input("What is this distance you want to travel in km?")
+# print(f"Your distance in meters is {int(distance)*1000}")
+
+# Write a program that takes the user's name and height(in centimeters), and outputs a summary sentence.
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
