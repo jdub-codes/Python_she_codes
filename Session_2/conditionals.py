f2c409d0d4dfc6dd87e13f102f2d8cb71e50ef31
jdub-codes <jenny.waller@riotinto.com>
2023-06-04 21:15:48 +1000

    Answers to list exercises

--- Session_2/conditionals.py
+++ Session_2/conditionals.py
@@ -34,7 +34,7 @@
 # print (False)
 
 # temperature = 20
-# # syntax (grammar) / semantics (logic) 
+# # # syntax (grammar) / semantics (logic) 
 # if temperature <= 18:
 #     print("Weather is too cold!")
 #     print("I want to say home")
@@ -49,12 +49,16 @@
 # else:
 #     print("Weather is nice!")
 
+# Question 1
+
 # Sara is a confident rock climber, but sometimes she forgets her helmet. 
 # Rei refuses to climb with Sara unless she's wearing a helmet, because That's Just Sensible.
 # Write a program that sets the value for a boolean variable called sara_has_helmet, and then prints out a string indicating whether or not Rei is down to climb.
 # sarah_has_helmet = True
 # "Let's send it!")
 # "No way, my brain is my moneymaker!"
+
+# My Answer
 
 # sarah_has_helmet = True
 
@@ -69,6 +73,8 @@
 # rei_has_rope = True
 # "Who's unprepared now, Rei?"
 # "I guess let's just go hiking?"
+
+# My Answers
 
 # sarah_has_helmet = True
 # rei_has_rope = True
@@ -110,14 +116,80 @@
 # else:
 #     print("I guess let's just go hiking?")
 
+# Question 3
+
 # Write a program that implements the algorithm for red light cameras. 
 # The program should print the string "Flash!" if (and only if) a car is detected driving while the light is red. 
 # If the light is green or amber, the program should print "Do nothing.", evenwhen a car is detected.
 
-light_color = "Red"
-car_detected = False
+# My Answer
 
-if car_detected and light_color == "Red":
-    print("Flash!")
-else:
-    print("Do nothing.")
+# light_color = "Red"
+# car_detected = False
+
+# if car_detected and light_color == "Red":
+#     print("Flash!")
+# else:
+#     print("Do nothing.")
+
+# Question 4
+
+# Write a program that asks the user for their height
+# and determines whether or not they are tall enough to ride the rollercoaster
+# which has a height requirement of 120cms.
+
+#120 "Hop on!"
+#50 "Sorry, not today."
+#165 "Hop on!"
+
+# height = 120
+# if height <= 165:
+#     print("Hop on!")
+# elif height >= 50:
+#     print("Sorry, not today.")
+# else:
+#     print("Hop on!")
+
+# Question 5
+
+# Write a program that asks the user to enter their username and password and output a success message if they are correct, or a failure message if they are incorrect. 
+# Your program has one user, whose username is "lucyg", and whose password is "quartzgleam1".
+
+# Input
+# "lucyg"
+# "quartzgleam?1"
+# Output
+# "Logged in successfully."
+
+# Input
+# "lucyg"
+# "diamondglow!2"
+# Output
+# "Access denied."
+
+# Input
+# "zacr"
+# "sirloinsteak3$"
+# Output
+# "Access denied."
+
+# lucyg = True
+# quartzgleam1 = True
+
+# if lucyg and quartzgleam1:
+#     print("Logged in successfully.")
+# else:
+#     print("Access denied.")
+
+# Question 6 
+
+# Write a program that asks the user to enter their email address and checks whether it is valid or not. 
+# For the purpose of this exercise, you can make the assumption that an email address is valid if it contains a “@” symbol and a “.” symbol.
+
+# Question 7
+
+# Will this script print anything to the terminal? Have a think first, decide on your answer, and then try running it to see if your intuition was correct.
+# If you get a different result to what you thought, see if you can come up with an explanation, and then check with a mentor.
+
+# if "False":
+#     print("A strange game. The only winning move is not to play.")
--- Session_2/lists.py
+++ Session_2/lists.py
@@ -1,5 +1,5 @@
 # Store multiple data points, can take different data types
-digits = [1,2,3,4,5] #stri,int,float,list
+# digits = [1,2,3,4,5] #stri,int,float,list
 # print(list_name)
 # print(type(list_name))
 
@@ -56,7 +56,8 @@ digits = [1,2,3,4,5] #stri,int,float,list
 
 # Exercises
 
-# Q1
+# Question 1
+
 # Given the list of foods below, print:
 # The first item in the list.
 # The third item in the list.
@@ -65,11 +66,69 @@ digits = [1,2,3,4,5] #stri,int,float,list
 # The last three items in the list.
 # The last item in the sublist.
 
-foods = ["orange","apple","banana","strawberry","grape","blueberry", ["carrot", "cauliflower", "pumpkin"],"passionfruit","mango","kiwifruit"]
+# foods = ["orange","apple","banana","strawberry","grape","blueberry", ["carrot", "cauliflower", "pumpkin"],"passionfruit","mango","kiwifruit"]
 
 # print(foods[0])
 # print(foods[2])
 # print(foods[-1])
 # print(foods[0:3])
 # print(foods[7:]) # Nested lists count as one of the sequence of numbers
-print(foods[10][2])
+# print(foods[6][2]) # Carrot is number 6 in the index and pumpkin is in position 2
+
+# Question 2
+
+# # Format and print the contents of the following list so that the output appears
+# A little bit of Monica in my life;
+# A little bit of Erica by my side;
+# A little bit of Rita's all I need;
+# A little bit of Tina's what I see;
+# A little bit of Sandra in the sun;
+# A little bit of Mary having fun;
+# A little bit of Jessica, here I am;
+# A little bit of you makes me your man (ah!)
+# *trumpet solo*
+
+lyrics = [["Monica", "in my life"],["Erica", "by my side"],["Rita's", "all I need"],["Tina's", "what I see"],["Sandra", "in the sun"],["Mary", "having fun"],["Jessica", "here I am"]]
+
+print("A little bit of",lyrics[0],";")
+print("A little bit of you makes me your name (ah!)")
+print("*trumpet solo*")
+
+#Question 3
+
+# Ask the user for three names. Add each name to a list, and then print the list.
+# Anteres
+# Rigel
+# Arcturus
+# ["Anteres", "Rigel", "Arcturus"]
+
+# Firstname = input("Type the name Anterest")
+# Secondname = input("Type the name Rigel")
+# Thirdname = input("Type the name Arcturus")
+
+# names = [Firstname, Secondname, Thirdname]
+# print(names)
+
+# Question 4
+
+# Using the following starter code:
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = [7, 8, 9]
+# d = []
+# e = []
+
+# Print the following lists:
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = [7, 8, 9]
+# d = []
+# e = []
+
+# print(a, b, c)
+
+# flatlist = (a + b + c)
+# print(flatlist)
