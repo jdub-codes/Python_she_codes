648e6f5c156f4e9869c756b0971d2d17ef4aebcf
jdub-codes <jenny.waller@riotinto.com>
2023-06-13 10:47:43 +1000

    Attempted functions Question 1

--- Session_3/loops.py
+++ Session_3/loops.py
@@ -72,16 +72,76 @@
 #     if number % 2 != 0:
 #         print(number, end = " ")
 
+# Oriyn's Answer
+
+
 # Question 3
 # Write a guessing game. Select a number, and save it as a variable in your code.
 # Ask the user to enter a number, and then output whether their number is less than or greater than the selected number. 
 # Keep asking until the user guesses the correct number. Then print a congratulatatory message.
 
-answer = 25
-guess = int(input("Guess a number:"))
-while guess == answer:
-    if guess > answer:
-        print("Too high...")
-    elif guess < answer:
+# answer = 25
+# guess = int(input("Guess a number:"))
+# while guess != answer:
+
+#     if guess > answer:
+#         print("Too high...")
+#     elif guess < answer:
+#         print("Too low...")
+#     elif guess == answer:
+#         print("You got it right")
+
+# Eve's answer
+
+print("Guess the random number")
+number = 76
+guess_attempt = input("Make a guess!")
+
+while(True):
+    if int(guess_attempt) < 76:
         print("Too low...")
-    print("You got it right!")
+        guess_attempt =  input("Make another attempt")
+    elif int(guess_attempt) >76:
+        print("Too high...")
+        guess_attempt = input("Make another attempt")
+    elif int(guess_attempt) == 76:
+        print("You get it right")
+        break
+
+# For Loops Exercises
+
+# Question 1
+
+# Ask the user for a number. Use a for loop to print the times tables for that number, up to ten:
+
+# numbers = [1,11]
+# to_multiply = int(input("Give me a number"))
+# for number in numbers:
+#     print(f"{to_multiply} x {number} = {to_multiply*number}")
+
+# Question 2
+
+# Ask the user for an integer. 
+# Using a for loop, add up every number from 0 up to that integer, and print the result.
+
+# max_number = int(input("Enter a number"))
+# total = 0
+
+# for number in range(0, max_number + 1):
+#     # print("This is the variable number first time"), number
+#     # print(total)
+#     total += number #0+1+2+3
+#     # print(number)
+#     # print(total)
+# print(str(total))
+
+# # Asli's answer
+
+# user_number = int(input"Enter a number")
+# list_of_numbers = []
+
+# for number in range(0, user_number+1):
+#     list_of_numbers.append(number)
+
+# total = sum(list_of_numbers)
+# print(total)
